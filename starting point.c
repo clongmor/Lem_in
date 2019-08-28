9ca15a985481dc32633dd0f703d0a0795c39860f
Chloe LONGMORE <clongmor@c3r4s6.wethinkcode.co.za>
2019-08-28 16:52:13 +0200

    add extras

--- starting point.c
+++ starting point.c
@@ -6,10 +6,11 @@
 /*   By: clongmor <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/27 16:49:39 by clongmor          #+#    #+#             */
-/*   Updated: 2019/08/27 16:51:22 by clongmor         ###   ########.fr       */
+/*   Updated: 2019/08/28 15:37:54 by clongmor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
 
 # define TRUE 1
 # define FALSE 0
@@ -32,3 +33,24 @@ typedef struct s_room
 	t_location		*coords;
 	struct s_room	*next;
 }				t_room;
+
+int		main(int argc, char **argv)
+{
+	int	read;
+	int	ant_no;
+	char *line;
+
+	if (!(line = (char*)malloc(sizeof(char)*3)));
+		return (0);
+	while ((read = get_next_line(0, &line)) == 1)
+	{
+		//do i need to use one line at a time or read all in then operate
+	}
+	//read from stdinput
+	//error checking
+	// create lists with coords
+	//add room links
+	//call algorithim with # of ants
+	//free lists
+	return (0);
+}
