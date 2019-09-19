fd3d7ded1d572392c56df7dadfb5dc846d06809d
Marthen van der Eems <marvan-d@c3r8s7.wethinkcode.co.za>
2019-09-19 13:02:57 +0200

    added buggy version of error that checks for valid room and ants

---
+++ srcs/debug.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc error_checking.c -Wall -Werror -Wextra -o debug ../libft/libft.a
--- srcs/error_checking.c
+++ srcs/error_checking.c
@@ -10,7 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include #include "../includes/lem.h"
+//#include #include "../includes/lem.h"
+#include "../libft/libft.h"
+#include "../libft/get_next_line.h"
 
 typedef struct  s_env
 {
@@ -19,17 +21,28 @@ typedef struct  s_env
     unsigned int    rooms : 1;
     unsigned int    start : 1;
     unsigned int    end : 1;
-    char            **rooms;
+    char            *room_lst;
     char            *line;
 }               t_env;
 
-static  void    ft_error(t_env *env)
+
+static  int ft_strnisdigit(char *str, int len)
 {
-    free(env->line);
-    //free(env);
-    ft_putendl_fd(2, "Error");
-    exit(0);
+	while (str[len] && len)
+	{
+		if (str[len] < 48 || str[len] > 57)
+			return (0);
+		len--;
+	}
+	return (1);
 }
+
+static  int valid_room(char *str)
+{
+    *str = '\0';
+    return (1);
+}
+
 
 static  int ft_strisdigit(char *str)
 {
@@ -42,9 +55,47 @@ static  int ft_strisdigit(char *str)
 	return (1);
 }
 
-static  void    validate_ants(t_env env)
+static  int validate_coords(char *line)
 {
-    while ((get_next_line(0, &env->line) < 1) && env->line[0] == '#' && env->line[1] != '#')
+    int space_indx;
+
+    space_indx = ft_strchr(line, ' ') - line;
+    if (!line || !space_indx)
+        return (0);
+    if (ft_strnisdigit(line, space_indx) && ft_strisdigit(line + space_indx + 1))
+        return (1);
+    return (0);
+}
+
+static  void    ft_error(t_env **env)
+{
+    free((*env)->line);
+    //free(env);
+    ft_putendl_fd("Error", 2);
+    exit(0);
+}
+
+static  int get_format(char *str)
+{
+    if (ft_strchr(str, ' '))
+    {
+        str += (ft_strchr(str, ' ') - str) + 1;
+        if (validate_coords(str))
+            return (1);
+        return (-1);
+    }
+    else if (str[0] == '#')
+        return (0);
+    else if (ft_strchr(str, '-'))
+        return (2);
+    return (-1);
+}
+
+
+
+static  void    validate_ants(t_env *env)
+{
+    while ((get_next_line(0, &(env->line)) < 1) && env->line[0] == '#' && env->line[1] != '#')
     if (!ft_strisdigit(env->line))
         ft_error(&env);
     if (ft_atoi(env->line) < 1)
@@ -53,20 +104,56 @@ static  void    validate_ants(t_env env)
     //free(env->line);
 }
 
-static  int     validate_rooms()
+static  void     validate_rooms(t_env *env)
 {
+    int cmd;
+    int format;
+
     //Unique name
     //Format [room name][single space][x_coord][single space][y_coord]
+    cmd = 0;
+    while (get_next_line(0, &(env->line)) > 0)
+    {
+        format = get_format(env->line);
+
+        if (format == -1)
+            ft_error(&env);
+        if (format == 0)
+        {
+            if (ft_strequ(env->line, "##start"))
+                cmd = 1;
+            if (ft_strequ(env->line, "##end"))
+                cmd = 2;
+        }
+        else if (cmd == 1 && format == 1)
+        {
+            env->start = 1;
+            cmd = 0;
+        }
+        else if (cmd == 2 && format == 1)
+        {
+            env->end = 1;
+            cmd = 0;
+        }
+        else if (format == 2)
+            break ;
+        else if (valid_room(env->line) == 1)
+            ft_error(&env);
+    }
+    if (env->room_lst != NULL)
+        env->rooms = 1;
 }
 
-void            read_input()
+int            read_input()
 {
     t_env env;
 
-    if (validate_ants(&env) && validate_rooms(&) && validate_links(&env))
-        
-    else
-        exit_and_free(&env);
+    validate_ants(&env);
+    validate_rooms(&env);
+
+    if (env.rooms && env.ants && env.start && env.end)
+        return (1);
+    return (0);
     /*
     while (get_next_line(0, &line) > 0)
 	{
@@ -90,4 +177,14 @@ void            read_input()
 	}
     return (elems_set.ants && elems_set.rooms && elems_set.links);
     */
+}
+
+
+int main()
+{
+    if (read_input() == 1)
+        ft_putendl("Valid file");
+    else   
+        ft_putendl("Invalid file");
+    return (0);
 }
